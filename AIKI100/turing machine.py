101e043ec129f8ab4b6356fb3f367d5006e2fb85
Balibber <88585231+Baliber@users.noreply.github.com>
2025-09-01 15:04:06 +0200

    ..

---
+++ AIKI100/turing machine.py
@@ -0,0 +1,16 @@
+input = "001"
+
+def q1(step):
+    if step >len(input):
+        print(input)
+        
+    elif input[step] == "0":
+        #Case
+        input[step] = "1"
+        q1(step+1)
+    elif input[step] == "1":
+        input[step] = "1"
+        q1(step+1)        
+
+
+q1(0)
