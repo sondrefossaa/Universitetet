7fbf393f8bc102c00a9c6c7713b172ae9d4802a9
Sondre <sondrefossaa@gmail.com>
2026-02-25 15:05:49 +0100

    continued working

---
+++ 2_termin/Algoritmeutvikling/uke_6/camels/camels.py
@@ -0,0 +1,8 @@
+from sys import stdin
+
+n = int(stdin.readline())
+
+Jaap = tuple(map(int, stdin.readline().split()))
+Jan = tuple(map(int, stdin.readline().split()))
+Thijs = tuple(map(int, stdin.readline().split()))
+print(Jaap, Jan, Thijs)
