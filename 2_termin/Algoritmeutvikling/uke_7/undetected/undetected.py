d9f7e4fc5be732dbf6664940fe1975834e1d1db5
sondrefossaa <sondrefossaa@gmail.com>
2026-03-04 13:58:22 +0100

    continued week 7

---
+++ 2_termin/Algoritmeutvikling/uke_7/undetected/undetected.py
@@ -0,0 +1,85 @@
+import sys
+
+input = sys.stdin.readline
+
+
+# check if any circles overlap from left to right
+def read_line_as_int():
+    return map(int, input().strip().split())
+
+
+field = [200, 300]
+
+
+class UnionFind:
+    def __init__(self, size) -> None:
+        self.comp = list(range(size))
+        self.rank = [0] * size
+
+    def find(self, u):
+        if self.comp[u] == u:
+            return u
+        else:
+            parent = self.find(self.comp[u])
+            self.comp[u] = parent
+            return parent
+
+    def union(self, u, v):
+        r1 = self.find(u)
+        r2 = self.find(v)
+        if self.rank[r1] < self.rank[r2]:
+            self.comp[r1] = r2
+        elif self.rank[r2] < self.rank[r1]:
+            self.comp[r2] = r1
+        else:
+            self.comp[r1] = r2
+            self.rank[r1] += 1
+
+    def print(self):
+        print(self.comp, self.rank)
+
+
+def dist(p1, p2):
+    return abs((p1[0] - p2[0])) + abs((p1[1] - p2[1]))
+
+
+def inside_circle(pos, circle):
+    x, y = pos
+    cx, cy, r = circle
+    return dist((x, y), (cx, cy)) <= r
+
+
+def check_overlap(c1, c2):
+    cx1, cy1, r1 = c1
+    cx2, cy2, r2 = c2
+    return dist((cx1, cy1), (cx2, cy2)) <= r1 + r2
+
+
+def overlaps_edge(c):
+    return (
+        c[0] - c[2] <= 0
+        or c[0] + c[2] >= field[0]
+        or c[1] - c[2] <= 0
+        or c[1] + c[2] >= field[1]
+    )
+
+
+def check_block(circles):
+    x, y = field
+
+
+k = int(input())
+
+circles = []
+uf = UnionFind(k * 2)
+# Add virtual edges in union find for the edges of the field
+for i in range(k):
+    circle = tuple(read_line_as_int())
+
+    # If circkle overlaps edge, connect it to the virtual edge in union find
+    # If it overlaps another circle, connect it to that circle in union find
+    # If the left and right virtual edges are connected, then the field is blocked
+    for ci in circles:
+        if check_overlap(circle, ci):
+            uf.union(circles.index(ci), i)
+    circles.append(circle)
